0f590f260a702938b5a829b517f272452a31f0f0
Cadie Stockman <cadie.brown@gmail.com>
2020-11-20 19:30:47 -0500

    Initial commit

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Handle all theme configuration here
+ **/
+namespace Diversity\Theme\Includes\Config;
+
+
+define( 'DIVERSITY_THEME_URL', get_stylesheet_directory_uri() );
+define( 'DIVERSITY_THEME_STATIC_URL', DIVERSITY_THEME_URL . '/static' );
+define( 'DIVERSITY_THEME_CSS_URL', DIVERSITY_THEME_STATIC_URL . '/css' );
+define( 'DIVERSITY_THEME_JS_URL', DIVERSITY_THEME_STATIC_URL . '/js' );
+define( 'DIVERSITY_THEME_IMG_URL', DIVERSITY_THEME_STATIC_URL . '/img' );
