d53d8f8fc5dc222c6e52a7a7ce52fd502a3027cd
Cadie Stockman <cadie.brown@gmail.com>
2021-01-09 22:28:50 -0500

    Display header_media on single.php

--- functions.php
+++ functions.php
@@ -7,5 +7,6 @@ define( 'DIVERSITY_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 // Theme foundation
 include_once DIVERSITY_THEME_DIR . 'includes/config.php';
 include_once DIVERSITY_THEME_DIR . 'includes/meta.php';
+include_once DIVERSITY_THEME_DIR . 'includes/post-functions.php';
 
 // Add other includes to this file as needed.
--- single.php
+++ single.php
@@ -9,6 +9,13 @@ $social = ( shortcode_exists( 'ucf-social-links' ) ) ? do_shortcode( '[ucf-socia
 ?>
 
 <article class="<?php echo $post->post_status; ?> post-list-item" aria-label="<?php echo esc_attr( get_the_title() ); ?>">
+	<div class="container mt-3 mt-sm-4">
+		<div class="row">
+			<div class="col-xl-10 offset-xl-1">
+				<?php echo $header_media; ?>
+			</div>
+		</div>
+	</div>
 	<div class="container mb-5 pb-sm-4">
 		<div class="row mb-4">
 			<div class="col-lg-10 offset-lg-1 px-lg-5 col-xl-8 offset-xl-2 px-xl-3">
