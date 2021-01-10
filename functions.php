964f57e097ce58965af353e72894cdd6bbac7961
Cadie Stockman <cadie.brown@gmail.com>
2021-01-09 21:18:48 -0500

    Update functions.php namespace

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
-namespace MyProject\Theme;
+namespace Diversity\Theme;
 
-define( 'MYPROJECT_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+define( 'DIVERSITY_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 
 
 // Theme foundation
-include_once MYPROJECT_THEME_DIR . 'includes/config.php';
-include_once MYPROJECT_THEME_DIR . 'includes/meta.php';
+include_once DIVERSITY_THEME_DIR . 'includes/config.php';
+include_once DIVERSITY_THEME_DIR . 'includes/meta.php';
 
 // Add other includes to this file as needed.
