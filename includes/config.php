e39f3e5af9de35ec32dc0c3370d3a9ce8ab8c156
Cadie Stockman <cadie.brown@gmail.com>
2021-01-10 17:23:46 -0500

    Update post header to utilize post featured image

--- includes/config.php
+++ includes/config.php
@@ -10,15 +10,3 @@ define( 'DIVERSITY_THEME_STATIC_URL', DIVERSITY_THEME_URL . '/static' );
 define( 'DIVERSITY_THEME_CSS_URL', DIVERSITY_THEME_STATIC_URL . '/css' );
 define( 'DIVERSITY_THEME_JS_URL', DIVERSITY_THEME_STATIC_URL . '/js' );
 define( 'DIVERSITY_THEME_IMG_URL', DIVERSITY_THEME_STATIC_URL . '/img' );
-
-
-/**
- * Hides the featured image metabox for standard posts in the WordPress admin.
- *
- * @since 1.0.0
- */
-function remove_post_thumbnail_box() {
-    remove_meta_box( 'postimagediv', 'post', 'side' );
-}
-
-add_action( 'do_meta_boxes', __NAMESPACE__ . '\remove_post_thumbnail_box' );
