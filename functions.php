0213aa678805a196ea0f16e263f64c54b703171e
Cadie Stockman <cadie.brown@gmail.com>
2021-01-15 01:04:04 -0500

    Add breadcrumbs

--- functions.php
+++ functions.php
@@ -9,5 +9,6 @@ include_once DIVERSITY_THEME_DIR . 'includes/config.php';
 include_once DIVERSITY_THEME_DIR . 'includes/meta.php';
 include_once DIVERSITY_THEME_DIR . 'includes/header-functions.php';
 include_once DIVERSITY_THEME_DIR . 'includes/post-functions.php';
+include_once DIVERSITY_THEME_DIR . 'includes/breadcrumb-functions.php';
 
 // Add other includes to this file as needed.
--- includes/config.php
+++ includes/config.php
@@ -10,3 +10,17 @@ define( 'DIVERSITY_THEME_STATIC_URL', DIVERSITY_THEME_URL . '/static' );
 define( 'DIVERSITY_THEME_CSS_URL', DIVERSITY_THEME_STATIC_URL . '/css' );
 define( 'DIVERSITY_THEME_JS_URL', DIVERSITY_THEME_STATIC_URL . '/js' );
 define( 'DIVERSITY_THEME_IMG_URL', DIVERSITY_THEME_STATIC_URL . '/img' );
+
+
+/**
+ * Initialization functions to be fired early when WordPress loads the theme.
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function init() {
+	// Adds support for Yoast-generated breadcrumbs.
+	add_theme_support( 'yoast-seo-breadcrumbs' );
+}
+
+add_action( 'after_setup_theme', __NAMESPACE__ . '\init', 11 );
